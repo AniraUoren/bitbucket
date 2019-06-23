4e70ebc72c68f4900dfba6f768abf1d9f3ccabab
Anira Uoren <Airin6666@yandex.ru>
2019-06-23 19:09:42 +0300

    App file

---
+++ task_1/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Table />
+
+  </div>
+</template>
+
+<script>
+import Table from './components/table.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Table
+  },
+}
+</script>
+
+<style lang="scss">
+</style>
